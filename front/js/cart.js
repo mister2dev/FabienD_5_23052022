6ddca3fac40ef21a6cc5b24675e6b28e15eb8eed
mister2dev <michelmaineime@hotmail.com>
2022-07-15 11:29:45 +0200

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,8 +6,67 @@ function main(){
 
 function displayCart (){
     //  test si il y a déjà des produits dans le local storage
-    if (localStorage.getItem("cart")){
-        let productCart = JSON.parse(localStorage.getItem("cart"));
-        console.log(productCart);
+    let productLocalStorage = JSON.parse(localStorage.getItem("cart"));
+
+    if (!productLocalStorage) {
+    
+        const titleCart = document.querySelector("h1");
+        const sectionCart = document.querySelector(".cart");
+    
+        titleCart.innerHTML = "Votre panier est vide !";
+        sectionCart.style.display = "none";
+    
+    } else {
+        fetch(`http://localhost:3000/api/products/${id}`)
+        .then(function(res){
+            return res.json();
+        })
+
+        for (let i=0; i < productLocalStorage.length; i++) {
+
+            // Création de la balise "article" et insertion dans la section
+            let productArticle = document.createElement("article");
+            document.querySelector("#cart__items").appendChild(productArticle);
+            productArticle.className = "cart__item";
+            productArticle.setAttribute("data-id", productLocalStorage[i].idKanap);
+            productArticle.setAttribute("data-color", productLocalStorage[i].color);
+
+             // Insertion de l'élément "div" pour l'image produit
+            let productDivImg = document.createElement("div");
+            productArticle.appendChild(productDivImg);
+            productDivImg.className = "cart__item__img";
+            
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productDivImg.appendChild(productImg);
+            productImg.src = productLocalStorage[i].imgKanap;
+            productImg.alt = ;
+
+            // Insertion de l'élément "div" pour la description produit
+            let productItemContent = document.createElement("div");
+            productArticle.appendChild(productItemContent);
+            productItemContent.className = "cart__item__content";
+
+            // Insertion de l'élément "div"
+            let productItemContentDescription = document.createElement("div");
+            productItemContent.appendChild(productItemContentDescription);
+            productItemContentDescription.className = "cart__item__content__Description";
+            
+            // Insertion du titre h2
+            let productTitle = document.createElement("h2");
+            productItemContentTitlePrice.appendChild(productTitle);
+            productTitle.innerHTML = ;
+
+            // Insertion de la couleur
+            let productColor = document.createElement("p");
+            productTitle.appendChild(productColor);
+            productColor.innerHTML = productLocalStorage[i].colorKanap;
+
+            // Insertion du prix
+            let productPrice = document.createElement("p");
+            productItemContentTitlePrice.appendChild(productPrice);
+            productPrice.innerHTML = ;
+
+        }    
     }
 }
