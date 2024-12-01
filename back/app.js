a84c9483aea60838ec55d0898afa60800f52f55c
mister2dev <michelmaineime@hotmail.com>
2024-12-01 04:00:39 +0100

    modif

--- back/app.js
+++ back/app.js
@@ -5,11 +5,24 @@ const productRoutes = require("./routes/product");
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://fabiend-5-23052022.onrender.com/"
+//   ); // Remplacez par l'URL de votre frontend
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
+
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://fabiend-5-23052022.onrender.com/"
-  ); // Remplacez par l'URL de votre frontend
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
@@ -20,13 +33,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.static("images"));
